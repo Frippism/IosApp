3c805ed0e2793a0faf0ea23731e3fc871a8bfba0
Ayush Tiwari <102849436+Frippism@users.noreply.github.com>
2023-10-05 16:14:19 +0530

    Mac

---
+++ dietPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dietPlanner
+//
+//  Created by Student on 05/10/23.
+//  Copyright © 2023 Lovely Profesional University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
